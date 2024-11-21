e595c2ddbef94de80d3d8d268ac246217bdb42d1
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-11-21 22:00:41 +0000

    feat(nvim): init

---
+++ .config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      plugins = {
+        -- wezterm = { enabled = true },
+        tmux = { enabled = true },
+      },
+    },
+  },
+  {
+    "folke/twilight.nvim",
+    opts = {},
+  },
+}
